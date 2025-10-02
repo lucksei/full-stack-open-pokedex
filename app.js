64da900818eabc27804b1137c775adde1eb33ab7
Luca Seimandi <lseimandi@colsecor.com.ar>
2025-10-02 15:19:40 -0300

    added version route to the backend, also a build_step.sh script for deploying

--- app.js
+++ app.js
@@ -4,6 +4,11 @@ const app = express();
 // get the port from env variable
 const PORT = process.env.PORT || 5000;
 
+// Version route
+app.get('/version', (req, res) => {
+  return res.send('1.0.0');
+});
+
 app.use(express.static('dist'));
 
 app.listen(PORT, () => {
---
+++ build_step.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Build Step"
+
+npm install
+npm run build
